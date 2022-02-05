fc461904d107166e033ea489c9d97f01f8662c9e
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-02-04 22:41:10 -0500

    added jquery script and quote and image constants

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// target quote speaker element
+const quoteSpeakerEl = $("#person");
+// target quote text element
+const quoteTextEl = $("#quoteContainer");
+// target photo image element
+const photoImgEl = $("#photoImgEl")
