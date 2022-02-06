79484d9ee8d7fe8711738c017b6fa174d50554ce
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-02-06 16:45:42 -0500

    dynamically called random emojis to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,10 @@ const quoteTextEl = $("#quoteText");
 const photoImgEl = $("#photoImgEl");
 
 // randon emoji array
-const emojiArray = ["🌻","💛","🍋","✨","🌼","🍯","🥞"];
-console.log(emojiArray);
+const emojiArray = ["🌻", "💛", "🍋", "✨", "🌼", "🍯", "🥞"];
+
+// target emoji span
+const randomEmojiEl = $("#randomEmoji");
 
 // target customize button
 const customizeSearch = $("#customizeSearch");
@@ -31,16 +33,16 @@ const submitSearch = $("#submitSearch");
 
 
 // toggle modal visibility 
-customizeSearch.click(function(){
+customizeSearch.click(function () {
     myModal.show();
-  });
+});
 
-modalClose.click(function(){
+modalClose.click(function () {
     myModal.hide();
 })
 
 // customize funciton
-submitSearch.click(function() {
+submitSearch.click(function () {
     // target author name
     let authorName = authorInputEl[0].value;
     console.log(authorName);
@@ -56,25 +58,48 @@ function getApi(authorName) {
     // quote garden api url
     const apiUrl = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${authorName}`
 
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            // target capitalized author name
-            console.log(data.data[0].quoteAuthor);
-            authorNameCap = data.data[0].quoteAuthor;
-            // target quote text
-            console.log(data.data[0].quoteText);
-            quoteText = data.data[0].quoteText;
 
-            quoteSpeakerEl.html("");
-            quoteSpeakerEl.html(authorNameCap);
+        fetch(apiUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                try {
+                console.log(data);
+                // target capitalized author name
+                console.log(data.data[0].quoteAuthor);
+                // randomize quotes and emojis
+                let i = Math.floor(Math.random() * 6 + 1)
+                let authorNameCap = data.data[i].quoteAuthor;
+                // target quote text
+                console.log(data.data[i].quoteText);
+                let quoteText = data.data[i].quoteText;
 
-            quoteTextEl.html("");
-            quoteTextEl.html(quoteText);
-        });
-}
+                quoteSpeakerEl.html("");
+                quoteSpeakerEl.html(authorNameCap);
 
+                quoteTextEl.html("");
+                quoteTextEl.html(quoteText);
+
+                let randomEmoji = emojiArray[i]
+                // trying to append random emoji
+                console.log(randomEmoji);
+                randomEmojiEl.text(randomEmoji);
+                } catch (error) {
+                    // display alert on screen "author not found"
+                    quoteSpeakerEl.html("Author not found. Try again!");
+                }
+            });
+    }
+
+const getAuthorRequest = "https://quote-garden.herokuapp.com/api/v3/authors"
+
+fetch(getAuthorRequest)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
 // api documentation https://pprathameshmore.github.io/QuoteGarden/
+// how to request all authors?
